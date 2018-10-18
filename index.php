349f8b13b98b0cc73cb981a548628db42d3c4720
aprinzhofer <aprinzhofer@mega.com>
2018-10-18 10:25:19 +0200

    premier test avec scss

--- index.php
+++ index.php
@@ -14,7 +14,7 @@
 	<script src="https://code.jquery.com/ui/1.12.1/jquery-ui.min.js"
 	  integrity="sha256-VazP97ZCwtekAsvgPBSUwPFKdrwD3unUfSGVYrahUqU="
 	  crossorigin="anonymous"></script>
-	<link rel="stylesheet" type="text/css" href="css/style.css">
+	<link rel="stylesheet" type="text/css" href="css/base.css">
 	
 
     <!--[if lt IE 9]>
